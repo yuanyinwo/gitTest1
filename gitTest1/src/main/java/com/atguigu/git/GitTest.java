0ca76ab6a8888b241f9235d114bdf680604ef9d3
liyang <3145516878@qq.com>
2022-01-17 14:13:14 +0800

    first commit

---
+++ gitTest1/src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author lystart
+ * @create 2022-01-17 14:12
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("git1");
+    }
+}
