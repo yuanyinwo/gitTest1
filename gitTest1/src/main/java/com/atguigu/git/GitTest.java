62c30cb8f6879beb19342baced54a90df2af0f24
liyang <3145516878@qq.com>
2022-01-17 14:20:00 +0800

    got-fix commit2

--- gitTest1/src/main/java/com/atguigu/git/GitTest.java
+++ gitTest1/src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git1");
         System.out.println("hot-fix");
+        System.out.println("hot-fix2");
     }
 }
